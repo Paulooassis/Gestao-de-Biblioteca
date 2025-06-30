6dc66cca07abe01db5f0649d86e36df665902b5c
Paulo Assis <passis@sga.pucminas.br>
2025-06-30 10:30:54 -0300

    .

--- demo/src/main/java/demo/BibliotecaDoPauloApplication.java
+++ demo/src/main/java/demo/BibliotecaDoPauloApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BibliotecaDoPauloApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BibliotecaDoPauloApplication.class, args);
 	}
 
