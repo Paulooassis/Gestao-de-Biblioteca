5ac54ef86d8f107a73f5265d38b11ee6b9e2c998
Paulo Assis <passis@sga.pucminas.br>
2025-06-30 10:22:47 -0300

    Add files via upload

---
+++ demo/demo/src/main/java/com/biblioteca/demo/BibliotecaDoPauloApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaDoPauloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaDoPauloApplication.class, args);
+	}
+
+}
---
+++ demo/demo/src/test/java/com/biblioteca/demo/BibliotecaDoPauloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaDoPauloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
